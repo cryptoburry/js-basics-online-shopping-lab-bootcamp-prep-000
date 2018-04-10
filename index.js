ed51265b750f5222cbd9c4ddd0430c515814d75c
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-04-10 02:22:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function total() {
 function removeFromCart(item) {
   for(var i=0;i<cart.length;i++){
     if(cart[i].itemName==item){
-      return cart.splice(i+1,1);
+      return cart.splice(i,1);
     }
   }
     return 'That item is not in your cart.';
