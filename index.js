ef0ff1c043e0042dacec78075d582b8b25130be7
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-04-10 02:25:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if(typeof cardNumber !== "undefined"){
+  if(cardNumber !== null){
     return "Sorry, we don't have a credit card on file for you.";
   }
   else{
